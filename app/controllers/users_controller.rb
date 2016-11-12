fd678f7b0e0075c01555ee619b04dc99710165c7
Nick Block <nicholasblock78@gmail.com>
2016-11-12 15:24:07 -0600

    able to register

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,22 @@ class UsersController < ApplicationController
 	end
 
 	def create
-		@user = User.new
+		@user = User.new(user_params)
 
-		
+		if @user.save
+			session[:user_id] = @user.id
+			redirect_to @user
+		else
+			render 'new'
+		end
+	end
+
+	def show
+		@user = User.find(params[:id])
 	end
 
 	private
 	def user_params
-		params.require(:user).permit()
+		params.require(:user).permit(:first_name, :last_name, :email, :password_digest)
 	end
 end
