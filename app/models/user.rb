774a103ee652daf4541a648bceb3ad176cc1ed69
Nick Block <nicholasblock78@gmail.com>
2016-11-16 09:44:58 -0600

    Successulyly able to add new user with Bcrypt funcitoning correctly

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,23 @@
+require 'bcrypt'
 class User < ApplicationRecord
-	has_secure_password
 
 	validates :email, uniqueness: true
 
+	def password
+		@password ||= BCrypt::Password.new(password_hash)
+	end
+
+	def password=(new_password)
+		@raw_password = new_password
+		@password = BCrypt::Password.create(new_password)
+		self.password_digest = @password
+	end
+
+	def self.authenticate(email, pass)
+	  	user_login = User.find_by(email: email) #returns user
+	  	if user_login && user_login.password == pass
+	  		return user_login
+	  	end
+		  	nil
+		end
 end
