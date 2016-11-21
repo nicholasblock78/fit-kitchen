052968fef565842e4a8918221a7c63e44f6f084d
Nick Block <nicholasblock78@gmail.com>
2016-11-21 03:12:10 -0600

    remove devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,6 @@
-require 'bcrypt'
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
 
-  devise :database_authenticatable, :registerable,
-  :recoverable, :rememberable, :trackable, :validatable
+  has_secure_password
 
   validates :email, uniqueness: true
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources 'users'
-  resources 'recipes'
+  resources 'users' do 
+    resources 'recipes'
+  end
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
