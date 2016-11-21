0f7ee1855f2cfb231d68a1799a9adf63764bd61c
Nick Block <nicholasblock78@gmail.com>
2016-11-21 04:20:52 -0600

    start to incorporate spoonacular api

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,15 @@
+require 'unirest'
+
 class PagesController < ApplicationController
 	def index
 		@recipes = Recipe.all
+		# These code snippets use an open-source library.
+		response = Unirest.get "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/search?limitLicense=false&number=10&offset=0&query=burger", 
+			headers:{
+				"X-Mashape-Key" => "nMARLGZymnmshppAMcfnST0uONvdp1lfM7XjsntqbAgkLWke3n", 
+				"Accept" => "application/json"
+			}	
+		
+		@returned = response.body
 	end
 end
